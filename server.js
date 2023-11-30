15f94309b36057a07bb1761af731c0347145d4fc
Justin Patterson <justin.patterson17@yahoo.com>
2023-11-29 21:50:52 -0400

    Game validation socket configured

--- server.js
+++ server.js
@@ -12,16 +12,52 @@ app.use(cors())
 
 
 io.on('connection',(socket)=>{
+    //console.log(socket.id)
     //socket.emit("get_id",socket.id)
     socket.on("join_room",(data)=>{
         socket.join(data)
-        socket.emit("receive_id",socket.id)
+       
+            // socket.emit("receive_id",{sign,socket})
+    
+        
+        //if()
+      
+        //console.log(io.of("/").adapter.rooms.size)
     })
     socket.on("send_message",(data)=>{
-        console.log(data)
+        //console.log(data)
         socket.to(data.roomNo).emit("receive_input",data)
         
         })
+
+        if(io.of("/").adapter.rooms.size === 1){
+            let sign = {sign: 'X',socket:socket.id,toPlay:true}
+            socket.emit("PlayerSign",sign)
+        }
+    
+        if(io.of("/").adapter.rooms.size > 1){
+            let sign ={sign: 'O',socket:socket.id,toPlay:false}
+            socket.emit("PlayerSign",sign)
+        }
+        console.log("io:"+io.of("/").adapter.rooms.size)
+
+
+    socket.on("playerTurn",data =>{
+       socket.to(data.roomNo).emit("PlayerTurnReturned",{toPlay:data.toPlay})
+    })
+    socket.on("disconnect",data=>{
+        console.log("User has left:" + socket.id)
+    })
+
+    socket.on("Game-Status",data=>{
+        if(data.GameResult){
+            io.sockets.emit('result',`Player ${data.player} has won`);
+        } else if(data.GameResult == false){
+            io.sockets.emit('result',`The Game has been squashed!!!`);
+        }
+
+        console.log(data)
+    })
     })
 
 
